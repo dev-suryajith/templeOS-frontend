8bcfdc86d671cf2cc68ce6d4bff2c8b390028cd3
dev-suryajith <suryajithss08@gmail.com>
2026-07-18 17:44:42 +0530

    Add Loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import NewReceipt from './pages/NewReceipt'
 import Admin from './pages/Admin'
+import Loader from './components/Loader'
 
 function App() {
   return (
     <div style={{ fontFamily: 'Gelasio' }} className='bg-linear-to-br from-orange-50 via-amber-100 to-orange-200'>
       <Routes>
+        <Route path='/' element={<Loader />} />
         <Route path='/login' element={<Login />} />
         <Route path='/generate-receipt' element={<NewReceipt />} />
         <Route path='/admin' element={<Admin />} />
---
+++ src/components/Loader.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Loader() {
+    const navigate=useNavigate()
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            navigate('/generate-receipt')
+        },500)
+    })
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-[#FFF9F2]">
+
+            <div className="text-center">
+
+                <div className="mx-auto flex h-28 w-28 animate-pulse items-center justify-center rounded-full bg-[#E39A2D] shadow-2xl">
+
+                    <img
+                        src="https://static.vecteezy.com/system/resources/thumbnails/005/200/720/small_2x/induism-symbol-om-sign-icon-black-color-illustration-flat-style-simple-image-vector.jpg"
+                        className="h-16 w-16 rounded-full"
+                        alt=""
+                    />
+
+                </div>
+
+                <h1 className="mt-8 font-gelasio text-3xl font-bold text-[#43200C]">
+                    TempleOS
+                </h1>
+
+                <p className="mt-2 text-[#C97A1B]">
+                    Loading...
+                </p>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Loader
